4490f308cd123730affbf76f959bf83ca54419b6
1 <martin@pixelpudu.com>
2018-01-31 16:10:22 +0000

    fix

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -30,3 +30,29 @@ function malvern_pingback_header() {
 	}
 }
 add_action( 'wp_head', 'malvern_pingback_header' );
+
+
+
+//This file only unsets the errors
+//You'll still need to override the app/views/checkout/form.php file to remove the password fields HTML
+//You can find instructions on overriding template files here: https://www.memberpress.com/1.1.7
+function unset_password_validation_errors($errors) {
+  if(empty($errors)) { return $errors; } //Should never happen if the password fields are hidden
+  //Unset the password field errors
+  foreach($errors as $i => $v) {
+    if($v == 'You must enter a Password.') {
+      unset($errors[$i]);
+    }
+    if($v == 'You must enter a Password Confirmation.') {
+      unset($errors[$i]);
+    }
+    if(stripslashes($v) == "Your Password and Password Confirmation don't match.") {
+      unset($errors[$i]);
+    }
+  }
+  //Artificially set a password here
+  $_POST['mepr_user_password'] = uniqid(); //Deprecated?
+  $_REQUEST['mepr_user_password'] = uniqid();
+  return $errors;
+}
+add_filter('mepr-validate-signup', 'unset_password_validation_errors');
