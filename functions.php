464d8105d14f3a60b90718e633e985fc5768bbfc
1 <martin@pixelpudu.com>
2018-02-07 12:11:29 +0000

    fix

--- functions.php
+++ functions.php
@@ -47,6 +47,7 @@ if ( ! function_exists( 'malvern_setup' ) ) :
 			'menu-1' => esc_html__( 'Primary', 'malvern' ),
 			'menu-2' => esc_html__( 'Footer', 'malvern' ),
 			'menu-3' => esc_html__( 'Course', 'malvern' ),
+			'menu-4' => esc_html__( 'Account', 'malvern' ),
 		) );
 
 		/*
